331fe3d3d40cf6290431491ec6418c14d356656b
shkim4548 <shkim4548@gmail.com>
2026-04-18 14:27:32 +0900

    크로스 플랫폼 작업 진행중

--- GW2_Server/GW2_CrossPlatformCore/Service.cpp
+++ GW2_Server/GW2_CrossPlatformCore/Service.cpp
@@ -1,2 +1,50 @@
 #include "pch.h"
 #include "Service.h"
+
+/*------------
+	Service
+--------------*/
+
+Service::Service(ServiceType type, NetAddress address, IoCoreRef core, SessionFactory factory, int32 maxSessionCount)
+	: _type(type), _netAddress(address), _ioCore(core), _sessionFactory(factory), _maxSessionCount(maxSessionCount)
+{
+
+}
+
+Service::~Service()
+{
+
+}
+
+void Service::CloseService()
+{
+
+}
+
+void Service::Broadcast(SendBufferRef sendBuffer)
+{
+	WRITE_LOCK;
+	for (const auto& session : _sessions)
+	{
+		session->Send(sendBuffer);
+	}
+}
+
+SessionRef Service::CreateSession()
+{
+	SessionRef session = _sessionFactory();
+	session->SetService(shared_from_this());
+	return session;
+}
+
+void Service::AddSession(SessionRef session)
+{
+	WRITE_LOCK;
+	_sessionCount++;
+	_sessions.insert(session);
+}
+
+void Service::ReleaseSession(SessionRef session)
+{
+	
+}
--- GW2_Server/GW2_CrossPlatformCore/Service.h
+++ GW2_Server/GW2_CrossPlatformCore/Service.h
@@ -16,7 +16,64 @@ using SessionFactory = function<SessionRef(void)>;
 
 class Service
 {
+public:
+	Service(ServiceType type, NetAddress address, IoCoreRef core, SessionFactory factory, int32 maxSessionCount = 1);
+	virtual ~Service();
 
-	
+	virtual bool	Start() = 0;
+	bool			CanStart() { return _sessionFactory != nullptr; }
+
+	virtual void	CloseService();
+	void			SetSessionFactory(SessionFactory func) { _sessionFactory = func; }
+	void			Broadcast(SendBufferRef sendBuffer);
+	SessionRef		CreateSession();
+	void			AddSession(SessionRef session);
+	void			ReleaseSession(SessionRef session);
+	int32			GetCurrentSessionCount() { return _sessionCount; }
+	int32			GetMaxSessionCount() { return _maxSessionCount; }
+
+public:
+	ServiceType		GetServiceType() { return _type; }
+	NetAddress		GetNetAddress() { return _netAddress; }
+	IoCoreRef&		GetIoCore() { return _ioCore; }
+
+protected:
+	USE_LOCK;
+	ServiceType _type;
+	NetAddress _netAddress = {};
+	IoCoreRef _ioCore;
+
+	Set<SessionRef> _sessions;
+	int32 _sessionCount = 0;
+	int32 _maxSessionCount = 0;
+	SessionFactory _sessionFactory;
 };
 
+/*------------------
+	ClientService
+--------------------*/
+class ClientService : public Service
+{
+public:
+	ClientService(NetAddress targetAddress, IoCoreRef core, SessionFactory factory, int32 maxSessionCount = 1);
+	virtual ~ClientService() { }
+	virtual bool Start() override;
+};
+
+/*-----------------
+	ServerService
+-------------------*/
+class ServerService : public Service
+{
+public:
+	ServerService(NetAddress targetAddress, IoCoreRef core, SessionFactory factory, int32 maxSessionCount = 1);
+	virtual ~ServerService() { }
+
+	virtual bool Start() override;
+	virtual void CloseService() override;
+	bool GetIsRunning() { return _isRunning; }
+
+private:
+	ListenRef _listener = nullptr;
+	Atomic<bool> _isRunning = false;;
+};
